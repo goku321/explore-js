b481cce1050992f34fd3c083a457671de6623418
Deepak <sah.sslpu@gmail.com>
2019-09-25 23:43:54 +0530

    Creating promise using constructor

--- promise/promise-basic.js
+++ promise/promise-basic.js
@@ -5,3 +5,11 @@ console.log(typeof(fifteen))
 // `then` registers a callback to be called when promise gets resolved
 fifteen.then(value => console.log(`Got ${value}`));
 // -> Got 15
+
+function returnPromise() {
+    return new Promise(resolve => {
+        setTimeout(() => { console.log('aFter timeout') }, 50);
+    })
+}
+
+obj = returnPromise();
